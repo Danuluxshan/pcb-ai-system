4c4c98acb19f61e8b6841ff9b7cd94ca422ff9a4
Danuluxshan <danuluxshan0121@outlook.com>
2026-06-27 15:59:20 +0530

    Project structure and dataset validation module initialized

---
+++ ml_training/scripts/validate_dataset.py
@@ -0,0 +1,24 @@
+"""
+=====================================================
+Project:
+AI-Powered Intelligent PCB Inspection System
+
+Module:
+Dataset Validation Utility
+
+Author:
+Danu Krish
+
+Description:
+This script validates YOLO datasets before training.
+
+Functions:
+- Count images
+- Count labels
+- Detect missing labels
+- Detect empty labels
+- Detect invalid annotations
+- Generate dataset statistics
+
+=====================================================
+"""
